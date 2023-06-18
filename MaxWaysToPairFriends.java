6c3e884f371f3cddf4f36de488b5322b56e3c179
Sahil Gohil <38555487+sahilgohil@users.noreply.github.com>
2023-06-18 20:41:39 +1000

    Create MaxWaysToPairFriends.java

---
+++ MaxWaysToPairFriends.java
@@ -0,0 +1,13 @@
+     public static int maxWaysToPairFriends(int n) {
+
+        int[] dp = new int[n+1];
+        
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i=3; i<dp.length;i++)
+        {
+            dp[i] = dp[i-1] + (i-1) * dp[i-2];
+        }
+
+        return dp[n];
+     }
